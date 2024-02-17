ed19471c9f011d238abe005756ca58d6956a3998
Shashank <saralayashashank@gmail.com>
2024-02-17 10:12:28 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		int ans = 0;
+		boolean f=true;
+		while(f) {
+			System.out.println("Enter a operator:");
+			char op = in.next().charAt(0);
+			if(op == '+'  || op == '-' || op == '*' || op == '/' || op == '%' ) {
+				System.out.print("Enter two numbers:");
+				int num1 = in.nextInt();
+				int num2 = in.nextInt();
+				switch(op) {
+				case('+')->ans=num1+num2;
+				case('-')->ans=num1-num2;
+				case('*')->ans=num1*num2;
+				case('/')->{
+					if(num2!=0) {
+					ans=num1/num2;}else{
+						System.out.println("Number cannot be divisible by zero");
+					}}
+				case('%')->ans=num1%num2;	
+				}
+				System.out.print("Answer: " + ans + "\n");
+			} else if(op == 'x' || op == 'X') {
+				break;
+			}else {
+				System.out.println("Invalid Character");
+			}
+		}
+	}
+
+}
