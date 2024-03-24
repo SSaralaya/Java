b1e41c508372faf56e4d5fa6797e69098005ac55
SSaralaya <shashanka0387@gmail.com>
2024-03-24 12:12:58 +0530

    Binary search on the ascending sorted array

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,7 +3,7 @@ public class BinarySearch {
 
 	public static void main(String[] args) {
 		int[] arr = {-18,-11,-10,-6,-3,0,1,3,5,8,10,11,19,20,27,30};
-		int target = 19;
+		int target = 1;
 		int ans = search(arr,target);
 		System.out.println("Index of the target element:" + ans);
 
@@ -13,19 +13,19 @@ public class BinarySearch {
 	//return -1 if the target element doesn't exists
 	static int search(int[] nums,int target) {
 		int start = 0;
-		int end = nums.length;
+		int end = nums.length-1;
 		while(start <= end) {
 			int mid = start + (end - start)/2;
-			if(nums[mid] == target) {
-				return mid;
-			}else if(target < nums[mid]) {
+			if(target < nums[mid]) {
 				end = mid - 1;
 			}else if(target > nums[mid]) {
 				start = mid + 1;
+			}else {
+				//Answer
+				return mid;
 			}
 		}
 		return -1;
 	}
 
 }
-
