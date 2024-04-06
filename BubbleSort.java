680f9aefc6a5856140849f21fe2c27229f228c22
SSaralaya <shashanka0387@gmail.com>
2024-04-06 14:31:52 +0530

    Sorting the array elements by Bubble Sort Method

---
+++ BubbleSort.java
@@ -0,0 +1,32 @@
+
+public class BubbleSort {
+
+	public static void bubbleSort(int[] arr,int n) {
+		for(int i = 0; i < n - 1; i++) {
+			for(int j = 0; j < n - i - 1; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void printArray(int[] arr, int n) {
+		for(int i = 0; i < n ; i++ ) {
+			System.out.print(arr[i]+ " ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] arr= {12,45,23,2,3,7,10,100,32};
+		int n = arr.length;
+		System.out.print("Array elements before sorting:");
+		printArray(arr,n);
+		bubbleSort(arr,n);
+		System.out.print("\nArray elements after sorting:");
+		printArray(arr,n);
+	}
+
+}
